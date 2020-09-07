31d610b919c91df9f410567d333adf9ebcf078f3
mikhailpopov <Romason13>
2020-09-07 16:15:43 +0300

    add Send to Event Hub function, get Data from Influx DB

---
+++ app.js
@@ -0,0 +1,56 @@
+const Influx = require('influx');
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('port', 3005);
+
+
+const influx = new Influx.InfluxDB({
+  host: 'qa-influxdb-test.onwizata.com',
+  database: 'telemetry',
+  username: 'wizata',
+  password: 'sVdBYsZGvMu3RhLqH2Jv6jUt',
+  protocol: 'https',
+  schema: [
+    {
+      measurement: 'sensors',
+      fields: {
+        value: Influx.FieldType.FLOAT
+      },
+      tags: [
+        'sensorId'
+      ]
+    }
+  ]
+});
+
+influx.getDatabaseNames()
+  .then(names => {
+    if (!names.includes('telemetry')) {
+      return error => console.log({error})
+    }
+  })
+  .then(() => {
+    app.listen(app.get('port'), () => {
+      console.log(`Listening on ${app.get('port')}.`);
+    });
+  })
+  .catch(error => console.log({ error }));
+
+app.get('/', (request, response) => {
+  const { place } = request.params;
+  influx.query(`
+    select * from sensors
+    limit 10
+  `)
+    .then(result => response.status(200).json(result))
+    .catch(error => response.status(500).json({ error }));
+});
---
+++ send.js
@@ -0,0 +1,33 @@
+const { EventHubProducerClient } = require("@azure/event-hubs");
+
+const connectionString = "Endpoint=sb://wiz-test-qa.servicebus.windows.net/;SharedAccessKeyName=fn;SharedAccessKey=z6WjJKcsWFWyosTs2YDiTd/4c1iX0SJq6jOzve38lvk=;";
+const eventHubName = "devicemessages";
+
+async function main(hardwareId, sensorValue) {
+
+  // Create a producer client to send messages to the event hub.
+  const producer = new EventHubProducerClient(connectionString, eventHubName);
+
+  // Prepare a batch of three events.
+  const batch = await producer.createBatch();
+  let date = new Date;
+  console.log(date.toISOString());
+  batch.tryAdd({ body: {
+    timestamp: date.toISOString(),
+      HardwareId: hardwareId,
+      SensorValue: sensorValue
+    } });
+
+
+  // Send the batch to the event hub.
+  await producer.sendBatch(batch);
+
+  // Close the producer client.
+  await producer.close();
+
+  console.log("A batch have been sent to the event hub");
+}
+
+main('test_4', 55).catch((err) => {
+  console.log("Error occurred: ", err);
+});
