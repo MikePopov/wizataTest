605f58958f23e99955b31f070b03c1ba3e9737eb
mikhailpopov <Romason13>
2020-09-08 14:54:09 +0300

    add deep equal for object

--- test/app.test.js
+++ test/app.test.js
@@ -22,8 +22,14 @@ describe('',  () => {
       .end((err, res) => {
         res.should.have.status(200);
         expect(res.body).to.have.lengthOf(10);
-        expect(res.body[0]).to.have.property('value').eq(3000);
-        expect(res.body[1]).to.have.property('value').eq(10000);
+        // expect(res.body[0]).to.have.property('value').eq(3000);
+        // expect(res.body[1]).to.have.property('value').eq(10000);
+        expect(res.body).to.deep.include({
+            time: '1754-08-30T22:43:41.128Z',
+            sensorId: '!!!!!!!!!!!_4_mult',
+            value: 3000
+          }
+        )
         console.log(res.body);
         done();
       });
