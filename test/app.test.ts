782a1cef52fc785f28b09f4bd603fe0af41ccc16
mikhailpopov <Romason13>
2020-09-09 17:14:19 +0300

    refactor

--- test/app.test.ts
+++ test/app.test.ts
@@ -3,27 +3,30 @@ import chaiHttp from "chai-http";
 import { expect } from "chai";
 import {app} from "../app";
 import chai from "chai"
+import exp from "constants";
 
 
 chai.use(chaiHttp);
 chai.should();
+chai.use(require('chai-like'));
+chai.use(require('chai-things'));
+
+const positiveTestData = [
+  {
+    HardwareId: 'TEST_TEST_1',
+    SensorValue: 20
+  },{
+    HardwareId: 'TEST_TEST_2',
+    SensorValue: -2
+  },{
+    HardwareId: 'TEST_TEST_3',
+    SensorValue: 0
+  }
+];
 
 
 describe('Data from which was send to Event Hub should correctly saved in InfluxDb',  () => {
   let date = new Date().toISOString();
-
-  const positiveTestData = [
-    {
-      HardwareId: 'TEST_TEST_1',
-      SensorValue: 20
-    },{
-      HardwareId: 'TEST_TEST_2',
-      SensorValue: -2
-    },{
-      HardwareId: 'TEST_TEST_3',
-      SensorValue: 0
-    }
-  ];
 
   positiveTestData.forEach(event => {
     before(async () => {
@@ -48,7 +51,43 @@ describe('Data from which was send to Event Hub should correctly saved in Influx
           done();
         });
     });
+
+    it(`sensorID = ${event.HardwareId} should saved correctly`,(done) => {
+      chai.request(app)
+        .get('/')
+        .end((err, res) => {
+          res.should.have.status(200);
+          expect(res.body.map(e=>(e.sensorId))).to.include(`${event.HardwareId}_mult`);
+          //console.log(res.body);
+          done();
+        });
+    });
+
+    it(`sensor value = ${event.SensorValue} should multiply on 2 = ${event.SensorValue*2} saved correctly`,(done) => {
+      chai.request(app)
+        .get('/')
+        .end((err, res) => {
+          res.should.have.status(200);
+          expect(res.body.map(e=>(e.value))).to.include(event.SensorValue*2);
+          //console.log(res.body);
+          done();
+        });
+    });
+
+    it.skip(`Time = ${date} should saved correctly`,(done) => {
+      chai.request(app)
+        .get('/')
+        .end((err, res) => {
+          res.should.have.status(200);
+          expect(res.body.map(e=>(e.time))).to.include(date);
+          //console.log(res.body);
+          done();
+        });
+    });
+
   });
+
+
 
   after(async () => {
     console.log('Clean DB...');
